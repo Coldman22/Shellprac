af8f3347c7fadcea55392639eaee3448a064fc21
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-11-13 15:55:12 +0530

    Create Variables1.sh

---
+++ Variables1.sh
@@ -0,0 +1,4 @@
+#Variables: It contains data inside in it.
+# There are two types of variable: System variable & User variables
+# System variables are created by unix/Linux systems inbuild created by operating system. (definied in captial cases)
+# User variables are created by users on the operating system. (defined by lower case)
